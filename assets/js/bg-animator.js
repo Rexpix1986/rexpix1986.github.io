a83aba9dd7255b27d0de2df6bd0bede1632fe481
Jett Pavlica <jettpav@gmail.com>
2026-07-03 22:16:44 -0400

    single osc bg splash w/ larger title card

--- assets/js/bg-animator.js
+++ assets/js/bg-animator.js
@@ -1,135 +1,44 @@
-var images = [];
-let imageCount = 9;
-while (imageCount--) {
-    images.push("assets/img/splash-bgs/bg-pic-" + (imageCount + 1) + ".webp")
-};
-images = shuffle(images);
-console.log(images);
+const image = document.querySelector(".bg-layer");
 
-
-const DISPLAY_TIME = 12000;
-const FADE_TIME = 2000;
-const MOVE_TIME = DISPLAY_TIME + FADE_TIME;
-
-let currentIndex = 0;
-
-let current = document.querySelector(".current");
-let next = document.querySelector(".next");
+const MOVE_TIME = 12000;
 
 function rand(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function randomTransform() {
+let pose = {
+    x: rand(-8, 8),
+    y: rand(-8, 8),
+    r: rand(-1, 1)
+};
 
-    return {
-        x1: rand(-8, 8),
-        y1: rand(-8, 8),
+function animateImage() {
 
-        x2: rand(-8, 8),
-        y2: rand(-8, 8),
+    image.getAnimations().forEach(a => a.cancel());
 
-        scale1: rand(1, 1.1),
-        scale2: rand(1.1, 1.2),
-
-        rotate1: rand(-1, 1),
-        rotate2: rand(-1, 1)
+    const next = {
+        x: rand(-8, 8),
+        y: rand(-8, 8),
+        r: rand(-1, 1)
     };
 
-}
-function animateImage(img) {
-
-    img.getAnimations().forEach(a => a.cancel());
-
-    const t = randomTransform();
-
-    img.animate([
+    image.animate([
         {
-            transform:
-                `translate(${t.x1}%, ${t.y1}%)
-                 rotate(${t.rotate1}deg)`
+            transform: `translate(${pose.x}%, ${pose.y}%) rotate(${pose.r}deg)`
         },
         {
-            transform:
-                `translate(${t.x2}%, ${t.y2}%)
-                 rotate(${t.rotate2}deg)`
+            transform: `translate(${next.x}%, ${next.y}%) rotate(${next.r}deg)`
         }
     ], {
         duration: MOVE_TIME,
-        easing: "linear",
+        easing: "ease-in-out",
         fill: "forwards"
+    }).finished.then(() => {
+        pose = next;
+        animateImage();
     });
 
 }
-function nextImage() {
 
-    currentIndex = (currentIndex + 1) % images.length;
-
-    next.src = images[currentIndex];
-    sizeImage(next);
-    animateImage(next);
-
-    requestAnimationFrame(() => {
-        next.classList.add("current");
-        current.classList.remove("current");
-    });
-
-    setTimeout(() => {
-
-        // swap the references only
-        [current, next] = [next, current];
-
-    }, FADE_TIME);
-
-}
-
-function start() {
-    document.documentElement.style.background = "transparent";
-    document.body.style.background = "transparent";
-
-    current.src = images[0];
-    current.classList.add("current");
-
-    sizeImage(current);
-    animateImage(current);
-
-    setInterval(nextImage, DISPLAY_TIME);
-
-}
-
-function sizeImage(img, overscan = 2) {
-    return;
-
-    const vw = window.innerWidth;
-    const vh = window.innerHeight;
-
-    const viewportAspect = vw / vh;
-    const imageAspect = img.naturalWidth / img.naturalHeight;
-
-    if (viewportAspect > imageAspect) {
-        // Wide viewport (desktop/laptop)
-        img.style.minWidth = `${vw * overscan}px`;
-        img.style.height = "auto";
-    } else {
-        // Tall viewport (phone)
-        img.style.width = "auto";
-        img.style.minHeight = `${vh * overscan}px`;
-    }
-}
-
-function shuffle(array) {
-    // Create a shallow copy if you don't want to mutate the original array
-    const shuffled = [...array];
-
-    for (let i = shuffled.length - 1; i > 0; i--) {
-        // Pick a random index from 0 to i
-        const j = Math.floor(Math.random() * (i + 1));
-
-        // Swap elements shuffled[i] and shuffled[j]
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-
-    return shuffled;
-}
-
-start();
+image.src = "clipsbackground.jpg";
+image.onload = animateImage;
